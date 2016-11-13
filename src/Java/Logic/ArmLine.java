c229204b83eda37ac2232363f0d48fd948bf6c98
Saber <SaberMeyt@gmail.com>
2016-11-13 21:51:00 +0300

    Initial commit

---
+++ src/Java/Logic/ArmLine.java
@@ -0,0 +1,40 @@
+package Logic;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Admin on 28.10.2016.
+ */
+
+/**
+ * Класс одного ребра алгоритма.Хранит текущий номер, усдлвие (предикат) и список операций.
+ */
+public class ArmLine {
+    /**
+     *
+     * @param currentArmNumber номер текущей вершины (для удобства обработки)
+     * @param condition экземпляр класса Condition, определяет логику прохождения
+     * @param statements список операций, выполняемых, если преликат condition истинный
+     */
+    public ArmLine(String currentArmNumber, Condition condition, ArrayList<Statement> statements) {
+        this.currentArmNumber = currentArmNumber;
+        this.condition = condition;
+        this.statements = statements;
+    }
+
+    /**
+     * Вывод на консоль содержимое ребра (метод toString() переопределять нет смысла)
+     */
+    public void printLine(){
+        //System.out.println("current arm: "+currentArmNumber);
+        System.out.println("conditon: "+"\n"+condition);
+        System.out.println("statements: ");
+        for(int i=0;i<statements.size();i++){
+            System.out.println(statements.get(i));
+        }
+    }
+    String currentArmNumber;
+    Condition condition;
+    ArrayList<Statement> statements;
+
+}
